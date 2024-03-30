b34612f7fbfb04aa3be4cd967e8fa822f6a3150a
Furi Sherpa <np03cs4a220013@heraldcollege.edu.np>
2024-03-30 21:34:12 +0545

    The navbar is updated

--- hms/src/App.js
+++ hms/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-
+import {  BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
+      <BrowserRouter>
       <Navbar/>
+      {/* <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/Dashboard" element={<Dashboard/>} />
+        <Route path="/HostelFacilities" element={<HostelFacilities/>} />
+        <Route path="/RoomAllocarion" element={<RoomAllocarion/>} />
+        <Route path="/Residents" element={<Residents/>} />
+        <Route path="/Payments" element={<Payments/>} />
+        <Route path="/Reports" element={<Reports/>} />
+        <Route path="/Settings" element={<Settings/>} />
+        <Route path="/LogOut" element={<LogOut/>} />
+        <Route path="/Login" element={<LoginSignup/>} />
+      </Routes> */}
+      </BrowserRouter>
     </div>
   )
 }
--- hms/src/Components/Navbar/Navbar.jsx
+++ hms/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './Navbar.css';
 import logo from '../Assets/logo.png';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const [menu, setMenu] = useState("Home");
@@ -13,15 +14,15 @@ const Navbar = () => {
       </div>
 
       <ul className="nav-menu">
-        <li className={menu === "Home" ? "active" : ""} onClick={() => { setMenu("Home") }}>Home</li>
-        <li className={menu === "Dashboard" ? "active" : ""} onClick={() => { setMenu("Dashboard") }}>Dashboard</li>
-        <li className={menu === "Hostel Facilities" ? "active" : ""} onClick={() => { setMenu("Hostel Facilities") }}>Hostel Facilities</li>
-        <li className={menu === "Room Allocation" ? "active" : ""} onClick={() => { setMenu("Room Allocation") }}>Room Allocation</li>
-        <li className={menu === "Residents" ? "active" : ""} onClick={() => { setMenu("Residents") }}>Residents</li>
-        <li className={menu === "Payments" ? "active" : ""} onClick={() => { setMenu("Payments") }}>Payments</li>
-        <li className={menu === "Reports" ? "active" : ""} onClick={() => { setMenu("Reports") }}>Reports</li>
-        <li className={menu === "Settings" ? "active" : ""} onClick={() => { setMenu("Settings") }}>Settings</li>
-        <li className={menu === "LogOut" ? "active" : ""} onClick={() => { setMenu("LogOut") }}>LogOut</li>
+      <Link style={{textDecoration: 'none'}} to="/"><li className={menu === "Home" ? "active" : ""} onClick={() => { setMenu("Home") }}>Home</li></Link>
+      <Link style={{textDecoration: 'none'}} to="/Dashboard"><li className={menu === "Dashboard" ? "active" : ""} onClick={() => { setMenu("Dashboard") }}>Dashboard</li></Link>
+      <Link style={{textDecoration: 'none'}} to="HostelFacilities"><li className={menu === "Hostel Facilities" ? "active" : ""} onClick={() => { setMenu("Hostel Facilities") }}>Hostel Facilities</li></Link>
+      <Link style={{textDecoration: 'none'}} to="/RoomAllocation"><li className={menu === "Room Allocation" ? "active" : ""} onClick={() => { setMenu("Room Allocation") }}>Room Allocation</li></Link>
+      <Link style={{textDecoration: 'none'}} to="/Residents"><li className={menu === "Residents" ? "active" : ""} onClick={() => { setMenu("Residents") }}>Residents</li></Link>
+      <Link style={{textDecoration: 'none'}} to="/Payments"><li className={menu === "Payments" ? "active" : ""} onClick={() => { setMenu("Payments") }}>Payments</li></Link>
+      <Link style={{textDecoration: 'none'}} to="/Reports"><li className={menu === "Reports" ? "active" : ""} onClick={() => { setMenu("Reports") }}>Reports</li></Link>
+      <Link style={{textDecoration: 'none'}} to="/Settings"><li className={menu === "Settings" ? "active" : ""} onClick={() => { setMenu("Settings") }}>Settings</li></Link>
+      <Link style={{textDecoration: 'none'}} to="/LogOut"><li className={menu === "LogOut" ? "active" : ""} onClick={() => { setMenu("LogOut") }}>LogOut</li></Link>
       </ul>
     </div>
   );
