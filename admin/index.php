19d5f346daeea99af5a64dc18e1d65c9f89235a3
Furi Sherpa <np03cs4a220013@heraldcollege.edu.np>
2024-04-12 06:46:04 +0545

    index.php for admin added

--- admin/index.php
+++ admin/index.php
@@ -1,105 +1,95 @@
 <?php
-session_start();
-include('../includes/dbconn.php');
-if (isset($_POST['login'])) {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $password = md5($password);
-    $stmt = $mysqli->prepare("SELECT username,email,password,id FROM admin WHERE (userName=?|| email=?) and password=? ");
-    $stmt->bind_param('sss', $username, $username, $password);
-    $stmt->execute();
-    $stmt->bind_result($username, $username, $password, $id);
-    $rs = $stmt->fetch();
-    $_SESSION['id'] = $id;
-    $uip = $_SERVER['REMOTE_ADDR'];
-    $ldate = date('d/m/Y h:i:s', time());
-    if ($rs) {
-        header("location:dashboard.php");
-    } else {
-        echo "<script>alert('Invalid Username/Email or password');</script>";
-    }
-}
+include("include/header.php");
 ?>
 
-<!DOCTYPE html>
-<html dir="ltr">
+<div class="container-fluid">
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicon.png">
-    <title>Hostel Management System</title>
-    <link href="../dist/css/style.min.css" rel="stylesheet">
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+    </div>
 
-    <script type="text/javascript">
-        function valid() {
-            if (document.registration.password.value != document.registration.cpassword.value) {
-                alert("Password and Re-Type Password Field do not match  !!");
-                document.registration.cpassword.focus();
-                return false;
-            }
-            return true;
-        }
-    </script>
+    <!-- Content Row -->
+    <div class="row">
 
-</head>
-
-<body>
-    <div class="main-wrapper">
-        <div class="preloader">
-            <div class="lds-ripple">
-                <div class="lds-pos"></div>
-                <div class="lds-pos"></div>
+        <!-- Total Employee Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                Total Employee</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">5</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="bi bi-person-square fa-2x text-gray-300 height='18px' width='18px'"></i>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="auth-wrapper d-flex no-block justify-content-center align-items-center position-relative" style="background:url(../assets/images/big/auth-bg.jpg) no-repeat center center;">
-            <div class="auth-box row">
-                <div class="col-lg-7 col-md-5 modal-bg-img" style="background-image: url(../assets/images/adimg.jpg);">
-                </div>
-                <div class="col-lg-5 col-md-7 bg-white">
-                    <div class="p-3">
-                        <div class="text-center">
-                            <img src="../assets/images/big/icon.png" alt="wrapkit">
-                        </div>
-                        <h2 class="mt-3 text-center">Admin Login</h2>
 
-                        <form class="mt-4" method="POST">
-                            <div class="row">
-                                <div class="col-lg-12">
-                                    <div class="form-group">
-                                        <label class="text-dark" for="uname">Email or Username</label>
-                                        <input class="form-control" name="username" id="uname" type="text" placeholder="Email or Username" required>
-                                    </div>
-                                </div>
-                                <div class="col-lg-12">
-                                    <div class="form-group">
-                                        <label class="text-dark" for="pwd">Password</label>
-                                        <input class="form-control" name="password" id="pwd" type="password" placeholder="Enter your password" required>
-                                    </div>
-                                </div>
-                                <div class="col-lg-12 text-center">
-                                    <button type="submit" name="login" class="btn btn-block btn-danger">LOGIN</button>
-                                </div>
-                                <div class="col-lg-12 text-center mt-5">
-                                    <a href="../index.php" class="text-danger">Go Back</a>
-                                </div>
-                            </div>
-                        </form>
+        <!-- Total Residents Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                Total Residents</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">20</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="bi bi-people-fill fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Total Rooms Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                Total Rooms</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">5</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="bi bi-house-fill fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Pending Requests Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-warning shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                Booking Requests</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">3</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-comments fa-2x text-gray-300"></i>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <script src="../assets/libs/jquery/dist/jquery.min.js "></script>
-    <script src="../assets/libs/popper.js/dist/umd/popper.min.js "></script>
-    <script src="../assets/libs/bootstrap/dist/js/bootstrap.min.js "></script>
 
-    <script>
-        $(".preloader ").fadeOut();
-    </script>
-</body>
+    <!-- Content Row -->
 
-</html>
+</div>
+
+<?php
+include("include/footer.php");
+?>
