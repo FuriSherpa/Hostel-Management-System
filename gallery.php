7ee9566c1aee6551f7d1b66949e4658a438fbb47
Furi Sherpa <np03cs4a220013@heraldcollege.edu.np>
2024-04-12 06:13:15 +0545

    gallery page done

---
+++ gallery.php
@@ -0,0 +1,36 @@
+<?php
+include("mainInclude/header.php");
+?>
+<!-- Page Content-->
+<section class="py-5">
+    <div class="container px-5 my-5">
+        <div class="row gx-5 justify-content-center">
+            <div class="col-lg-6">
+                <div class="text-center mb-5">
+                    <h1 class="fw-bolder">Gallery</h1>
+                    <p class="lead fw-normal text-muted mb-0">
+                        Welcome to our Gallery Page! Here, you'll find a visual journey through the vibrant experiences and memorable moments captured within our hostel. Browse through our collection of photos showcasing our cozy accommodations, inviting communal spaces, and the vibrant atmosphere that awaits you at HostelStays. </p>
+                </div>
+            </div>
+        </div>
+        <div class="row gx-5">
+            <div class="col-12"><img class="img-fluid rounded-3 mb-5" src="images/1.jpg" alt="..." /></div>
+            <div class="col-lg-6"><img class="img-fluid rounded-3 mb-5" src="images/2.jpg" alt="..." /></div>
+            <div class="col-lg-6"><img class="img-fluid rounded-3 mb-5" src="images/3.jpg" alt="..." /></div>
+        </div>
+        <div class="row gx-5 justify-content-center">
+            <div class="col-lg-6">
+                <div class="text-center mb-5">
+                    <a class="text-decoration-none" href="#!">
+                        View All
+                        <i class="bi-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+include("mainInclude/footer.php");
+?>
