2960373ff0d56b5c66ec39ccebdd6609a971ecfe
Raksha Khadka <np03cs4a220498@heraldcollege.edu.np>
2024-04-12 06:18:35 +0545

    about page added

---
+++ about.php
@@ -0,0 +1,88 @@
+<?php
+include("mainInclude/header.php");
+?>
+<header class="py-5">
+    <div class="container px-5">
+        <div class="row justify-content-center">
+            <div class="col-lg-8 col-xxl-6">
+                <div class="text-center my-5">
+                    <h1 class="fw-bolder mb-3">Our mission is to provide more than just accommodations.</h1>
+                    <p class="lead fw-normal text-muted mb-4">We aim to offer an unforgettable experience that enriches the lives of our guests. We're committed to creating a home away from home where people can relax, explore, and connect with like-minded individuals. Through exceptional hospitality and genuine care, we strive to inspire a sense of belonging and adventure in everyone who walks through our doors.</p>
+                    <a class="btn btn-primary btn-lg" href="#scroll-target">Read our story</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- About section one-->
+<section class="py-5 bg-light" id="scroll-target">
+    <div class="container px-5 my-5">
+        <div class="row gx-5 align-items-center">
+            <div class="col-lg-6"><img class="img-fluid rounded mb-5 mb-lg-0" src="images/story.jpg" alt="..." /></div>
+            <div class="col-lg-6">
+                <h2 class="fw-bolder">Our Story</h2>
+                <p class="lead fw-normal text-muted mb-0">At HostelStays, our story began with a simple idea: to create a welcoming space where people far from their home could come together, share their experiences, and forge lasting connections. Founded by a group of passionate individuals, we envisioned a place where the spirit of exploration thrives, and every guest feels like part of our vibrant community.</p>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- About section two-->
+<section class="py-5">
+    <div class="container px-5 my-5">
+        <div class="row gx-5 align-items-center">
+            <div class="col-lg-6 order-first order-lg-last"><img class="img-fluid rounded mb-5 mb-lg-0" src="images/values.jpg" alt="..." /></div>
+            <div class="col-lg-6">
+                <h2 class="fw-bolder">Our Values</h2> 
+                <ul class="values">
+                    <li class="lead fw-normal text-muted mb-0"><strong>Inclusivity:</strong> We welcome guests from all backgrounds and cultures, fostering an environment of diversity and mutual respect.</li>
+                    <li class="lead fw-normal text-muted mb-0"><strong>Community:</strong> We believe in the power of community, where every guest is valued and encouraged to share their unique stories and perspectives.</li>
+                    <li class="lead fw-normal text-muted mb-0"><strong>Sustainability:</strong> We are dedicated to minimizing our environmental impact and promoting eco-friendly practices throughout our operations.</li>
+                    <li class="lead fw-normal text-muted mb-0"><strong>Excellence:</strong> We are committed to delivering outstanding service and continuously striving for excellence in all aspects of our hostel.</li>   
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Team members section-->
+<section class="py-5 bg-light">
+    <div class="container px-5 my-5">
+        <div class="text-center">
+            <h2 class="fw-bolder">L5CG2</h2>
+            <p class="lead fw-normal text-muted mb-5">Team-B Hostel Management System</p>
+        </div>
+        <div class="row gx-5 row-cols-1 row-cols-sm-2 row-cols-xl-4 justify-content-center">
+            <div class="col mb-5 mb-5 mb-xl-0">
+                <div class="text-center">
+                    <img class="img-fluid rounded-circle mb-4 px-4" src="https://dummyimage.com/150x150/ced4da/6c757d" alt="..." />
+                    <h5 class="fw-bolder">Ankit Bogati</h5>
+                    <div class="fst-italic text-muted">Project Manager</div>
+                </div>
+            </div>
+            <div class="col mb-5 mb-5 mb-xl-0">
+                <div class="text-center">
+                    <img class="img-fluid rounded-circle mb-4 px-4" src="images/team/pujan.png" alt="..." height="200px" width="200px"/>
+                    <h5 class="fw-bolder">Pujan Pant</h5>
+                    <div class="fst-italic text-muted">Business Analyst</div>
+                </div>
+            </div>
+            <div class="col mb-5 mb-5 mb-sm-0">
+                <div class="text-center">
+                    <img class="img-fluid rounded-circle mb-4 px-4" src="images/team/raksha.jpg" alt="..." height="200px" width="200px"/>
+                    <h5 class="fw-bolder">Raksha Khadka</h5>
+                    <div class="fst-italic text-muted">Developer</div>
+                </div>
+            </div>
+            <div class="col mb-5">
+                <div class="text-center">
+                    <img class="img-fluid rounded-circle mb-4 px-4" src="https://dummyimage.com/150x150/ced4da/6c757d" alt="..." />
+                    <h5 class="fw-bolder">Furi Sherpa</h5>
+                    <div class="fst-italic text-muted">Developer</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+include("mainInclude/footer.php");
+?>
