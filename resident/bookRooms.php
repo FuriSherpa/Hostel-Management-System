7ff4edbb61f67e25b4b08f5f2d641280ef4f07fc
Raksha Khadka <np03cs4a220498@heraldcollege.edu.np>
2024-04-19 07:06:30 +0545

    bookRooms added with only header and footer

---
+++ resident/bookRooms.php
@@ -0,0 +1,30 @@
+<?php
+// Start session if not already started
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+include('include/header.php');
+
+// Include database connection
+include("../mainInclude/dbConn.php");
+
+if (isset($_SESSION["is_login"])) {
+    $rLogEmail = $_SESSION['rLogEmail'];
+} else {
+    echo  "<script> location.href='../index.php'; </script>";
+}
+?>
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800">Book Rooms</h1>
+
+</div>
+<!-- /.container-fluid -->
+
+<?php
+include("include/footer.php");
+?>
