1a65939f3e3efce3f123c736cbfaef921d0d4ce9
Raksha Khadka <np03cs4a220498@heraldcollege.edu.np>
2024-05-10 17:43:53 +0545

    Room viewing feature is added.

---
+++ staff/viewRooms.php
@@ -0,0 +1,30 @@
+<?php
+// Start session if not already started
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+include('include/header.php');
+
+// Include database connection
+include("../mainInclude/dbConn.php");
+
+if (isset($_SESSION["is_staff_login"])) {
+    $sEmail = $_SESSION['sEmail'];
+} else {
+    echo  "<script> location.href='../index.php'; </script>";
+}
+?>
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800">View Rooms</h1>
+
+</div>
+<!-- /.container-fluid -->
+
+<?php
+include("include/footer.php");
+?>
