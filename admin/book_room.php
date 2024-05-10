73f3c66dbb7d82915c0f431374e4389f06d888d6
Raksha Khadka <np03cs4a220498@heraldcollege.edu.np>
2024-05-10 08:36:07 +0545

    the booking is attached

---
+++ admin/book_room.php
@@ -0,0 +1,26 @@
+<?php
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+include("include/header.php");
+include("../mainInclude/dbConn.php");
+
+if (!isset($_SESSION['is_admin_login'])) {
+    echo "<script> location.href='../index.php'; </script>";
+    exit;
+}
+?>
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800">Book Room</h1>
+
+</div>
+<!-- /.container-fluid -->
+
+<?php
+include("include/footer.php");
+?>
